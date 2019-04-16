93a52af8c2b02292e9e4eee6faab121fbd8418e5
harshvardhan58 <harshvardhansingh58@gmail.com>
2019-04-16 12:10:31 +0530

    Optimizing Code by removing for loop and passing parent node as argument

--- js/ques1.js
+++ js/ques1.js
@@ -1,38 +1,29 @@
-var Counter = function (countercount) {
+var Counter = function (countercount,node) {
     var count=0;
-    var node = document.getElementById('ques1');
     var elem=document.getElementsByClassName("counter")[0].cloneNode(true);
     elem.classList.remove('d-none');
     var tmp = elem.getElementsByTagName('button');
     var inp = elem.getElementsByTagName('input')[0];
     elem.getElementsByClassName('countercount')[0].innerHTML=countercount;
     inp.value=count;
-    for(var i=0;i<tmp.length;i++){
-        var arr=tmp[i].className.split(" ")
-        if(arr.includes('delete')){
-            tmp[i].addEventListener('click',function(){
-                node.removeChild(elem);
-            });
-        }
-        if(arr.includes('increment')){
-            tmp[i].addEventListener('click',function(){
-                inp.value = ++count;
-            });
-        }
-        if(arr.includes('decrement')){
-            tmp[i].addEventListener('click',function(){
-                inp.value = --count;
-            });
-        }
-    }
+    tmp[2].addEventListener('click',function(){
+        node.removeChild(elem);
+    });
+    tmp[1].addEventListener('click',function(){
+        inp.value = ++count;
+    });
+    tmp[0].addEventListener('click',function(){
+        inp.value = --count;
+    });
     node.appendChild(elem)
      
 }
 
 var addNewCounter = function(){
     var countercount=0;
+    var node = document.getElementById('ques1');
     return function(){
-        Counter(++countercount);
+        Counter(++countercount,node);
     }    
 }();
 
